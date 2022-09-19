446cb7a5d95e2d7bad1be5f7723f0a8a5ad28394
ramachandran g <ramachandrang@wekan.company>
2022-09-19 13:25:46 +0530

    Adding -y to the shell script

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 
 #!/bin/sh
 
-sudo npm install -g mongodb-realm-cli
+# sudo npm install -g mongodb-realm-cli
 
-realm-cli login --api-key iiujwobk --private-api-key 3a84beaf-9abb-4e21-9861-7ffc1efe30a3
+npx mongodb-realm-cli login --api-key iiujwobk --private-api-key 3a84beaf-9abb-4e21-9861-7ffc1efe30a3
 
-realm-cli push --remote facilitron-circleci-wscpe
+npx mongodb-realm-cli push -y --remote facilitron-circleci-wscpe 
 
 
